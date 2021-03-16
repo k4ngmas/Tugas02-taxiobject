cbea4635c5448a2615585c6869b86b1213d637d2
k4ngmas <68710943+k4ngmas@users.noreply.github.com>
2021-03-16 14:42:17 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            coding1 taxi = new coding1();
+
+            taxi.drivername = "Bobby";
+            taxi.onduty = true;
+            taxi.numpassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.pickuppassenger();
+            taxi.dropoffpassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ coding1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Program
+{
+    public class coding1
+    {
+        public string drivername { get; set; }
+        public bool onduty { get; set; }
+        public int numpassenger { get; set; }
+        
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Nama Driver :{0}", drivername);
+            if (onduty == true)
+            {
+                Console.WriteLine("Taxi On Duty : Yes");
+            }
+            if (onduty == false)
+            {
+                Console.WriteLine("Taxi On Duty : No");
+            }
+            Console.WriteLine("Jumlah Of Penumpang: {0}", numpassenger);
+        }
+
+        public void pickuppassenger()
+        {
+            Console.WriteLine("{0} Sedang Menjemput Penumpang", drivername);
+        }
+        public void dropoffpassenger()
+        {
+            Console.WriteLine("{0} Selesai Mengantarkan Penumpang", drivername);
+        }
+    }
+}
